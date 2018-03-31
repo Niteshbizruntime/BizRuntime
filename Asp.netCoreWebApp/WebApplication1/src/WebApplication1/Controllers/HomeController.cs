2edcc1937fd254ad29c76f613b70f2cdc9f4fddd
Nitesh Bansal <you@example.com>
2018-03-31 18:22:56 +0530

    ASP.NET CORE

---
+++ Asp.netCoreWebApp/WebApplication1/src/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
