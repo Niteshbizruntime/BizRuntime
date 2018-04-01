e6f674f2eed9a649df04a444a0ce48850a09bdc2
Nitesh Bansal <you@example.com>
2018-04-02 01:43:44 +0530

    All

---
+++ All/Json.js
@@ -0,0 +1,153 @@
+﻿   var str="";
+  
+   
+
+   function Call()
+   {
+    var username = document.New.register_name.value;
+    var password = document.New.register_password.value;
+    var phone = document.New.phone.value;
+    var email = document.New.register_email.value;
+    var confpass = document.New.con_register_password.value;
+    var confemail = document.New.con_register_email.value;
+    var date = document.New.register_date.value;
+    var gender = document.New.register_gender.value;
+    var country = document.New.register_country.value;
+
+    if (grecaptcha.getResponse() == "")
+    {
+        alert("Please Verify Captcha");
+        return false;
+    }
+
+  
+
+    var num = Math.ceil((Math.random() * 10));
+    var otp = num == 1 ? num * 5555 : num * 555;
+    alert("Generated OTP "+otp);
+
+    var v = prompt("Enter OTP");
+    if (v == otp)
+    {
+        
+        try
+        {
+            if (localStorage.getItem('obj2') != null)
+            {
+                
+                str = localStorage.getItem('obj2')+"|";
+              
+               
+            }
+            var data1 = {username,password,phone,email,date,gender,country };
+            window.str = str+JSON.stringify(data1);
+           localStorage.setItem('obj2', window.str);
+            alert("Account Create Successful");
+            open("loginPage.html");
+        }
+        catch (e)
+        {
+            alert("Error: " + e.description);
+        }
+        
+    }
+    else
+    {
+        alert("Enter Correct OTP");
+        v = prompt("Enter OTP");
+    }
+   }
+
+   function Call1()
+   {
+
+    var username = document.Forget.register_name.value;
+    var phone = document.Forget.phone.value;
+    var email = document.Forget.register_email.value;
+    var confemail = document.Forget.con_register_email.value;
+    var password = "";
+
+    window.str = localStorage.getItem('obj2');
+    var data=str.split("|");
+    
+    var l =0;
+    for (var i = 0; i < data.length; i++)
+    {
+        try
+        {
+            var call = JSON.parse(data[i]);
+            if (username == call["username"] && phone == call["phone"] && email == call["email"])
+            { l = 1; password = call["password"]; break;}
+            else
+                l=0;
+            
+        }
+        catch (e)
+        {
+            alert("Error: " + e.description);
+        }
+       
+    }
+
+   
+
+    if (grecaptcha.getResponse() == "")
+    {
+        alert("Please Verify Captcha");
+        return false;
+    }
+
+    var num = Math.ceil((Math.random() * 10));
+    var otp = num == 1 ? num * 5555 : num * 555;
+    alert("Generated OTP " + otp );
+
+    var v = prompt("Enter OTP");
+    if (v == otp)
+    {
+         if(l==1)
+            alert("Your Password is " + password);
+        else
+             alert("Enter Correct Detail If U have Account");
+         
+    }
+    else
+    {
+        alert("Enter Correct OTP");
+        v = prompt("Enter OTP");
+    }
+   }
+
+   function validateform()
+   {
+    if (grecaptcha.getResponse() == "")
+    {
+        alert("Please Verify Captcha");
+        return false;
+    }
+
+    var username = document.Login.username.value;
+    var password = document.Login.pass.value;
+
+    window.str = localStorage.getItem('obj2');
+    var data = str.split("|");
+
+    var l = 0;
+    for (var i = 0; i < data.length; i++) {
+        try {
+            var call = JSON.parse(data[i]);
+            if (username == call["username"] && password == call["password"]) {
+                alert("Login Successfull");
+                open("HomePage.html");
+            }
+            else
+                l = 0;
+
+        }
+        catch (e) {
+            alert("Error: " + e.description);
+        }
+         
+    }
+    if (l == 0)
+        alert("Enter Correct UserName and Password");
+   }
---
+++ All/Validation1.js
@@ -0,0 +1,162 @@
+﻿
+var pass = "";
+var email = "";
+var username = "";
+
+function Other(data)
+{
+    if (data == 'other')
+        document.getElementById('other').innerHTML = "<tr><th>"+"<label style=\"width:170px\" for=\"other\" class=\"label\">Country Name</label>"+"</th><td>"+" <input type=\"couname\" name=\"couname\" value=\"\" id=\"other\" style=\"width:170px\" required />"+"</td> </tr>";
+}
+function check()
+{
+    var str = localStorage.getItem('obj2');
+    var data = str.split("|");
+
+    var l = 0;
+    for (var i = 0; i < data.length; i++) {
+        try {
+            var call = JSON.parse(data[i]);
+            if (username == call["username"])
+            { l = 1;  break; }
+            else
+                l = 0;
+
+        }
+        catch (e) {
+            alert("Error: " + e.description);
+        }
+        
+
+    }
+    return (l == 1 ? true : false);
+}
+
+
+function isTheFieldEmpty(inputField, helpId) {
+
+    
+    username = inputField.value;
+    if(check())
+    {
+         if (helpId != null)
+        // We need to show a warning
+        // Remove any warnings that may exist
+        while (helpId.childNodes[0]) {
+            helpId.removeChild(helpId.childNodes[0]);
+        }
+
+    // Add new warning
+    helpId.appendChild(document.createTextNode("UserName Already exist"));
+    }
+    else
+    return editNodeText(/^[A-Za-z0-9]{6,15}$/, inputField.value, helpId, "Please enter a valid name At least 6 letter.");
+}
+
+
+
+// inputField.value – Value typed in the html text box
+function isPassword(inputField, helpId) {
+    pass = inputField.value;
+    return editNodeText(/(?=.*\d)(?=.*[@ $ # & * ])(?=.*[A-Z])(?=.*[a-z]).{8,12}$/, inputField.value, helpId, "Enter Valid Password at least 8 Charcter(Ndfga@13)");
+}
+
+function isSamePass(inputField, helpId) {
+
+
+    if (inputField.value != pass) {
+        if (helpId != null)
+            // We need to show a warning
+            // Remove any warnings that may exist
+            while (helpId.childNodes[0]) {
+                helpId.removeChild(helpId.childNodes[0]);
+            }
+
+        // Add new warning
+        helpId.appendChild(document.createTextNode("Enter Same Password"));
+    }
+    else {
+
+        // If the right information was entered, clear the help message
+        if (helpId != null) {
+
+            // Remove any warnings that may exist
+            while (helpId.childNodes[0]) {
+                helpId.removeChild(helpId.childNodes[0]);
+            }
+
+        }
+    }
+
+}
+
+function isSameEmail(inputField, helpId) {
+
+
+    if (inputField.value != email) {
+        if (helpId != null)
+            // We need to show a warning
+            // Remove any warnings that may exist
+            while (helpId.childNodes[0]) {
+                helpId.removeChild(helpId.childNodes[0]);
+            }
+
+        // Add new warning
+        helpId.appendChild(document.createTextNode("Enter Same Email"));
+    }
+    else {
+
+        // If the right information was entered, clear the help message
+        if (helpId != null) {
+
+            // Remove any warnings that may exist
+            while (helpId.childNodes[0]) {
+                helpId.removeChild(helpId.childNodes[0]);
+            }
+
+        }
+    }
+}
+
+function isPhoneOk(inputField, helpId) {
+
+    return editNodeText(/^[6-9][0-9]{9}$/, inputField.value, helpId, "Enter 10 Digit Valid Phone Number");
+
+}
+
+function isEmailOk(inputField, helpId) {
+    email = inputField.value;
+    return editNodeText(/^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}$/, inputField.value, helpId, "Enter an Email like derekbanas@gmail.com");
+
+}
+
+function editNodeText(regex, input, helpId, helpMessage) {
+    // See if the info matches the regex that was defined
+    // If the wrong information was entered, warn them
+    if (!regex.test(input)) {
+
+        if (helpId != null)
+            // We need to show a warning
+            // Remove any warnings that may exist
+            while (helpId.childNodes[0]) {
+                helpId.removeChild(helpId.childNodes[0]);
+            }
+
+        // Add new warning
+        helpId.appendChild(document.createTextNode(helpMessage));
+
+    } else {
+
+        // If the right information was entered, clear the help message
+        if (helpId != null) {
+
+            // Remove any warnings that may exist
+            while (helpId.childNodes[0]) {
+                helpId.removeChild(helpId.childNodes[0]);
+            }
+
+        }
+
+    }
+}
+
