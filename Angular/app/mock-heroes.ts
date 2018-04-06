7e9be233d8d726a33b069b9cb59ccbca243463ec
Nitesh Bansal <you@example.com>
2018-04-06 20:25:09 +0530

    Angular

---
+++ Angular/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './Hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Pawan klayan' },
+  { id: 12, name: 'AA' },
+  { id: 13, name: 'Prabas' },
+  { id: 14, name: 'Rana' },
+  { id: 15, name: 'Chiru' },
+  { id: 16, name: 'Nani' },
+  { id: 17, name: 'jr Ntr' },
+  { id: 18, name: 'venkatesh' },
+  { id: 19, name: 'Ram' },
+  { id: 20, name: 'Surya' }
+];
